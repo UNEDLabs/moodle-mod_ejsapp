5cc79a3e33b92af98e5f3e0dec8127e230e3e32f
Luis de la Torre <ldelatorre@dia.uned.es>
2013-02-22 13:45:55 +0100

    Loading state files bugfix v. 1.4

--- generate_applet_embedding_code.php
+++ generate_applet_embedding_code.php
@@ -207,11 +207,15 @@ function generate_applet_embedding_code($ejsapp, $sarlabinfo, $state_file, $coll
         $state_fail_msg = get_string('state_fail_msg', 'ejsapp');
         $load_state_code = "var applet = document.getElementById('{$ejsapp->applet_name}');
           function performAppletCode(count) {
-	        if (!applet.isActive() && count > 0) {
+	          if (!applet._readState && count > 0) {
 	            window.setTimeout( function() { performAppletCode( --count ); }, 2000 );
             }
-            else if (applet.isActive()) {
+            else if (applet._readState) {
               applet._readState('url:$state_file');
+              //applet._view.resetTraces();
+              //applet._view.clearData();
+              //applet._view.clearElements();
+              //applet._view.resetElements();
             }
             else {
               alert('$state_fail_msg');
