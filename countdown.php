29e2fa0d22dc4b41ca353a6ba51b697d37eaa055
Luis de la Torre <ldelatorre@dia.uned.es>
2015-04-27 12:23:00 +0200

    More javascript options related with the time of use of remote labs

---
+++ countdown.php
@@ -0,0 +1,45 @@
+<?php
+
+// This file is part of the Moodle module "EJSApp"
+//
+// EJSApp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// EJSApp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// The GNU General Public License is available on <http://www.gnu.org/licenses/>
+//
+// EJSApp has been developed by:
+//  - Luis de la Torre: ldelatorre@dia.uned.es
+//	- Ruben Heradio: rheradio@issi.uned.es
+//
+//  at the Computer Science and Automatic Control, Spanish Open University
+//  (UNED), Madrid, Spain
+
+/**
+ * Ajax update for the EJSApp view.php when a user needs to wait for a remote lab to be available
+ *
+ * @package    mod
+ * @subpackage ejsapp
+ * @copyright  2012 Luis de la Torre and Ruben Heradio
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+
+require_login(0, false);
+
+$remaining_time = required_param('remaining_time', 'int');
+
+global $PAGE;
+
+$PAGE->set_context(context_system::instance());
+$PAGE->set_url('/mod/ejsapp/countdown.php');
+
+if ($remaining_time > 0) echo $remaining_time . ' ' . get_string('seconds', 'ejsapp');
+else echo get_string('refresh', 'ejsapp');
