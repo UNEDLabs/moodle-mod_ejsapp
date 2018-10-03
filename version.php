d229be9f630c3781826c54dcac5dcfb01b18ba57
Luis de la Torre <ldelatorre@dia.uned.es>
2018-10-03 09:48:17 +0200

    Updates version

--- version.php
+++ version.php
@@ -31,8 +31,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018071000;         // The current module version (Date: YYYYMMDDXX).
+$plugin->version  = 2018100300;         // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2013111800;
 $plugin->component = 'mod_ejsapp';      // To check on upgrade, that module sits in correct place.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1 (Build: 2018071000)';
+$plugin->release = '3.1 (Build: 2018100300)';
